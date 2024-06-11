826fe95e7fd2c149b69a06792d2ace2706ca3821
jean-baptiste demonte <jbdemonte@gmail.com>
2024-06-11 15:22:09 +0200

    Generic gamepad (#1)
    * create a generic gamepad allowing to specify its keys
    
    * make generic functions public
    
    * make version configurable

---
+++ generic_gamepad.go
@@ -0,0 +1,74 @@
+package uinput
+
+import (
+	"fmt"
+	"os"
+)
+
+// CreateGamepad will create a new gamepad using the given uinput
+// device path of the uinput device.
+func CreateGenericGamepad(path string, name []byte, vendor uint16, product uint16, version uint16, keys []uint16, absEvents []uint16) (Gamepad, error) {
+	err := validateDevicePath(path)
+	if err != nil {
+		return nil, err
+	}
+	err = validateUinputName(name)
+	if err != nil {
+		return nil, err
+	}
+
+	fd, err := createVGenericGamepadDevice(path, name, vendor, product, version, keys, absEvents)
+	if err != nil {
+		return nil, err
+	}
+
+	return vGamepad{name: name, deviceFile: fd}, nil
+}
+
+func createVGenericGamepadDevice(path string, name []byte, vendor uint16, product uint16, version uint16, keys []uint16, absEvents []uint16) (fd *os.File, err error) {
+	deviceFile, err := createDeviceFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create virtual gamepad device: %v", err)
+	}
+
+	// register button events
+	err = registerDevice(deviceFile, uintptr(evKey))
+	if err != nil {
+		_ = deviceFile.Close()
+		return nil, fmt.Errorf("failed to register virtual gamepad device: %v", err)
+	}
+
+	for _, code := range keys {
+		fmt.Printf("registering uiSetKeyBit 0x%x\n", code)
+		err = ioctl(deviceFile, uiSetKeyBit, uintptr(code))
+		if err != nil {
+			_ = deviceFile.Close()
+			return nil, fmt.Errorf("failed to register key number %d: %v", code, err)
+		}
+	}
+
+	// register absolute events
+	err = registerDevice(deviceFile, uintptr(evAbs))
+	if err != nil {
+		_ = deviceFile.Close()
+		return nil, fmt.Errorf("failed to register absolute event input device: %v", err)
+	}
+
+	for _, event := range absEvents {
+		fmt.Printf("registering uiSetAbsBit 0x%x\n", event)
+		err = ioctl(deviceFile, uiSetAbsBit, uintptr(event))
+		if err != nil {
+			_ = deviceFile.Close()
+			return nil, fmt.Errorf("failed to register absolute event %v: %v", event, err)
+		}
+	}
+
+	return createUsbDevice(deviceFile,
+		uinputUserDev{
+			Name: toUinputName(name),
+			ID: inputID{
+				Bustype: busUsb,
+				Vendor:  vendor,
+				Product: product,
+				Version: version}})
+}
